cf00927547236401d535211bcd1975e40540eded
Manish0027 <manish.p@tridhyatech.com>
2023-01-19 10:59:19 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component,OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'FORM';
+  
+  ismarried = false;
+  private formSubmitAttempt !: boolean;
+ @ViewChild('contactForm') contactform! : NgForm ;
+   
+  ngOnInit(){
+
+  }
+  signUp(){
+    // console.log(this.contactform.value);
+    // this.contactform.reset();
+    this.formSubmitAttempt = true;
+    if (this.contactform.valid) {
+      console.log('form submitted');
+      console.log(this.contactform.value);
+    }
+  }
+  reset() {
+    this.contactform.reset();
+    this.formSubmitAttempt = false;
+  }
 }
+ 
